53d4705d0fe001316da7596aadc2cc2b3f09aed8
DPAHiK <artiom.medvedev2005@yandex.ru>
2024-10-07 23:31:33 +0300

    added userdetails and userdetails service

--- src/main/java/com/example/blog/models/User.java
+++ src/main/java/com/example/blog/models/User.java
@@ -11,4 +11,36 @@ public class User {
     private String name;
     private String password;
     private String roles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
 }
