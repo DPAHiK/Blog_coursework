bd11219725177a16942f525114b68ad64f7920b3
DPAHiK <artiom.medvedev2005@yandex.ru>
2024-10-08 00:21:00 +0300

    modified post service and main controller

--- src/main/java/com/example/blog/controllers/MainController.java
+++ src/main/java/com/example/blog/controllers/MainController.java
@@ -1,11 +1,27 @@
 package com.example.blog.controllers;
 
+import com.example.blog.models.Post;
+import com.example.blog.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private PostService service;
+
+    public MainController(PostService postService) {
+        this.service = postService;
+    }
 
     @GetMapping("/")
     public String home(Model model) {
@@ -27,6 +43,75 @@ public class MainController {
     @GetMapping("/registration")
     public String registration(Model model) {
         return "registration";
+    }
+
+    @GetMapping("/blog")
+    public String blogMain(Model model){
+        Iterable<Post> posts = service.allPosts();
+        model.addAttribute("posts", posts);
+        return "blog-main";
+    }
+
+    @GetMapping("/blog/add")
+    public String blogAdd(Model model){
+        return "blog-add";
+    }
+
+    @PostMapping("/blog/add")
+    public String blogPostAdd(@RequestParam String title,
+                              @RequestParam String anons,
+                              @RequestParam String full_text,
+                              Model model ){
+        Post post = new Post(title, anons, full_text);
+        service.addPost(post);
+        return "redirect:/blog";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String blogDetails(Model model, @PathVariable(value = "id") long id){
+
+        Optional<Post> post = service.postByID(id);
+        if (service.postByID(id).isEmpty()) return "redirect:/blog";
+
+        ArrayList<Post> res = new ArrayList<Post>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "blog-details";
+    }
+
+    @GetMapping("/blog/{id}/edit")
+    public String blogEdit(Model model, @PathVariable(value = "id") long id){
+        Optional<Post> post = service.postByID(id);
+        if (service.postByID(id).isEmpty()) return "redirect:/blog";
+
+        ArrayList <Post> res = new ArrayList<Post>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "blog-edit";
+    }
+
+    @PostMapping("/blog/{id}/edit")
+    public String blogPostUpdate(@RequestParam String title,
+                                 @RequestParam String anons,
+                                 @RequestParam String full_text,
+                                 @PathVariable(value = "id") long id,
+                                 Model model ){
+        Post post = service.postByID(id).orElseThrow();
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFull_text(full_text);
+        service.addPost(post);
+
+        return "redirect:/blog";
+    }
+
+    @PostMapping("/blog/{id}/remove")
+    public String blogPostRemove(@PathVariable(value = "id") long id,
+                                 Model model ){
+        Post post = service.postByID(id).orElseThrow();
+        service.deletePost(post);
+        
+        return "redirect:/blog";
     }
 
 }
--- src/main/java/com/example/blog/services/PostService.java
+++ src/main/java/com/example/blog/services/PostService.java
@@ -1,42 +1,56 @@
 package com.example.blog.services;
 
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import com.example.blog.repo.PostRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.example.blog.models.Post;
 import com.example.blog.models.User;
 import com.example.blog.repo.UserRepository;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
+import java.util.Optional;
 
 @Service
 public class PostService {
-    private List<Post> posts;
-    private UserRepository repository;
+    private Iterable<Post> posts;
+    private UserRepository userRepository;
+    private PostRepository postRepository;
     private PasswordEncoder passwordEncoder;
 
-    public PostService(List<Post> posts, UserRepository repository) {
-        this.posts = posts;
-        this.repository = repository;
-        this.passwordEncoder = new BCryptPasswordEncoder();
+    public PostService(UserRepository userRepository, PostRepository postRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.postRepository = postRepository;
+        this.passwordEncoder = passwordEncoder;
+        posts = postRepository.findAll();
     }
 
-    public List<Post> allPosts() {
+    public Iterable<Post> allPosts() {
         return posts;
     }
 
-    public Post postByID(int id) {
-        return posts.stream()
-                .filter(post -> post.getId() == id)
-                .findFirst()
-                .orElse(null);
+    public Optional<Post> postByID(long id) {
+        Optional<Post> post = Optional.empty();
+        for (Post post1 : posts) {
+            if (post1.getId() == id) {
+                post = Optional.of(post1);
+            }
+        }
+        return post;
     }
 
     public void addUser(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        repository.save(user);
+        userRepository.save(user);
+    }
+
+    public void addPost(Post post) {
+        postRepository.save(post);
+        //add to posts too
+    }
+
+    public void deletePost(Post post) {
+        postRepository.delete(post);
     }
 }
