c5921618536724b428f76936194b3cc8dbbd8edb
DPAHiK <artiom.medvedev2005@yandex.ru>
2024-10-13 18:25:33 +0300

    added comment repo

---
+++ src/main/java/com/example/blog/repo/CommentRepository.java
@@ -0,0 +1,12 @@
+package com.example.blog.repo;
+
+import com.example.blog.models.Comment;
+import jakarta.transaction.Transactional;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CommentRepository extends CrudRepository<Comment, Long> {
+    Iterable<Comment> findByPostId(Long post_id);
+
+    @Transactional
+    void deleteByPostId(Long post_id);
+}
