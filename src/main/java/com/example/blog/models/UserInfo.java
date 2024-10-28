1966c2737f79cbb313491d139002677b2fc00438
DPAHiK <artiom.medvedev2005@yandex.ru>
2024-10-28 23:35:31 +0300

    added user info entity

--- src/main/java/com/example/blog/models/UserInfo.java
+++ src/main/java/com/example/blog/models/UserInfo.java
@@ -1,4 +1,62 @@
 package com.example.blog.models;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
 public class UserInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String regDate;
+    private Long postCount;
+    private Long commentCount;
+
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+
+    public UserInfo() {
+    }
+
+    public UserInfo(Long id, String regDate, Long postCount, Long commentCount) {
+        this.id = id;
+        this.regDate = regDate;
+        this.postCount = postCount;
+        this.commentCount = commentCount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(String regDate) {
+        this.regDate = regDate;
+    }
+
+    public Long getPostCount() {
+        return postCount;
+    }
+
+    public void setPostCount(Long postCount) {
+        this.postCount = postCount;
+    }
+
+    public Long getCommentCount() {
+        return commentCount;
+    }
+
+    public void setCommentCount(Long commentCount) {
+        this.commentCount = commentCount;
+    }
 }
